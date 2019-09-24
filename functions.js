08d84dda29a3dff0f8fc7a435a95e364ec46c978
Markus <markus.sallinen@float.fi>
2019-09-24 09:00:44 +0300

    added mirror

--- functions.js
+++ functions.js
@@ -7,8 +7,8 @@ function zerofill(s){
     //returns seconds padded if needed
 }
 test('Zerofill 1', t => {t.is(zerofill(5), "05")})
-test('Zerofill 1', t => {t.is(zerofill(0), "00")})
-test('Zerofill 1', t => {t.is(zerofill(50), "50")})
+test('Zerofill 2', t => {t.is(zerofill(0), "00")})
+test('Zerofill 3', t => {t.is(zerofill(50), "50")})
 
 /**
  * 
@@ -43,6 +43,25 @@ function yearsOld(born){
 test('Yearsold 1', t => {t.is(yearsOld(2000), 19)})
 test('Yearsold 2', t => {t.is(yearsOld(1975), 44)})
 test('Yearsold 3', t => {t.is(yearsOld(1800), 219)})
+/**
+ * 
+ * @param {array} words 
+ */
+function concatenate(words){
+    //returns sentence with space between words
+}
+test('Concatenate 1', t => {t.is(concatenate(["En","groda","hoppar"]), "En groda hoppar")})
+test('Concatenate 2', t => {t.is(concatenate(["Kungen","har","kommit","till","stan"]), "Kungen har kommit till stan")})
+/**
+ * 
+ * @param {string} word 
+ */
+function mirror(word){
+    //returns parameter
+}
+
+test('Mirror 1', t => {t.is(concatenate("Gädda"), "Gädda")})
+test('Mirror 2', t => {t.is(concatenate("Kulpåse"), "Kulpåse")})
 
 
 
